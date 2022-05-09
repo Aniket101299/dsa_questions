b2cde0d6cac78e5d392e710dbb1e1a5225779542
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-05-09 08:25:44 +0530

    object value que done

---
+++ FindingValueInsideObject.js
@@ -0,0 +1,47 @@
+/*
+- Write method findPath
+- Should take two params:
+    - object
+    - keys separated by dots as string
+- Return value if it exists at that path inside the object, else return undefined
+*/
+
+var obj = {
+    a: {
+        b: {
+            c: 12,
+            j: false
+        },
+        k: null
+    }
+};
+
+const findPath = (object, path) => {
+    
+    path = path.split("."); 
+    let i=0;
+    let newObj = object;
+    
+    while(i<path.length) {
+        
+        if(typeof newObj== "boolean" || typeof newObj== "string" || typeof newObj== "number" || newObj== "null") {
+                return undefined;
+        } else {
+           newObj = newObj[path[i]];   
+        }
+        
+        i++;
+    }
+     return newObj;
+};
+
+
+console.log(findPath(obj, 'a.b.c')); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
