44a3f2dcbdb867b93f6101587776b1e24bab3ce2
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-05-27 00:00:12 +0530

    done

---
+++ goodBadString.js
@@ -0,0 +1,51 @@
+function runProgram(input) {
+    input = input.trim().split("\n");
+    let T= +input[0]; let line=1;
+    for(let i=0; i<T; i++){
+        let arr= input[line++].trim().split("");
+        console.log(GoodBad(arr));
+    }
+}
+function GoodBad(arr){
+    let ans='';
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i]!=arr[i+1]){
+            // arr.splice(i,1);
+            ans+=arr[i];
+        }
+        
+    }
+    ans+=arr[arr.length-1];
+    return ans;
+}
+
+
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
+
+
+
+
+
+
+
+
