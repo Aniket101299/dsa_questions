7f9d44525f301211b7b2b3a4166bd2855a15ec31
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-06-29 11:46:03 +0530

    add another optimise way

--- getUniqueProducts.js
+++ getUniqueProducts.js
@@ -6,7 +6,8 @@ You have been given a list of products which is having property productName, qua
 
 PROBLEM STATEMENTS:
 
-1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+1. you need to write a function say, getUniqueProductCount which should return count
+ of each Product(as an object) present in the given list of Products considering Product Name as Key.
 
 Sample Output for the given listOfProducts will be :
 
@@ -18,7 +19,9 @@ Sample Output for the given listOfProducts will be :
 
 
 
-2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+2. you need to write a function say, getUniquePrducts which should return an array of objects 
+by grouping the products based on the productName and summing up the quantity for the same products 
+present in the given list of Products considering Product Name as Key.
 
 Sample Output for the given listOfProducts will be :
 
@@ -71,7 +74,25 @@ const listOfProducts = [{
   }
 ];
 
-function getUniquePrducts() {
+
+
+function getUniqueProductCount(listOfProducts) {
+    let obj = {};
+    listOfProducts.map((product) => {
+        if(obj[product.productName] === undefined) {
+            obj[product.productName] = 1;
+        } else {
+            obj[product.productName] = obj[product.productName] + 1;
+        }
+    })
+
+    return obj;
+}
+
+console.log(getUniqueProductCount(listOfProducts));
+
+
+function getUniquePrducts(listOfProducts) {
 let obj = {};
 let uniqueArr = [];
 listOfProducts.map((product) => {
@@ -99,6 +120,74 @@ return uniqueArr;
 
 }
 
-console.log(getUniquePrducts());
+console.log(getUniquePrducts(listOfProducts));
   
+
+
+// another optimise way 
+
+
+
+
+//  function, getUniqueProductCount which return count of each Product
+//  as an object present in the given list of Products considering Product Name as Key
+
+
+function getUniqueProductCount(listOfProducts) {
+
+    let obj = {};
+
+    listOfProducts.forEach((product) => {
+
+        if( obj[product.productName] === undefined ) {
+            obj[product.productName] = 1;
+        } else {
+            obj[product.productName] = obj[product.productName] + 1;
+        }
+
+    });
+
+    return obj;
+}
+
+console.log(getUniqueProductCount(listOfProducts));
+
+
+
+
+
+//  function, getUniquePrducts which return an array of objects by grouping the products
+//  based on the productName and summing up the quantity for the same products 
+//  present in the given list of Products considering Product Name as Key.
+
+
+function checkAndUpdate(arr, product) {
+
+    let respectiveIndex = arr.findIndex( (el) => (el.productName == product.productName) );
+   
+    if( respectiveIndex == -1 ) {
+        arr.push(product);
+    } else {
+        arr[respectiveIndex].quantity = arr[respectiveIndex].quantity + product.quantity;
+    }
+      return arr;
+}
+
+
+
+function getUniquePrducts(listOfProducts) {
+
+    let OutputArr = new Array;
+   
+    for (let i = 0; i < listOfProducts.length; i++) {
+
+        checkAndUpdate(OutputArr, listOfProducts[i]);
+
+    } 
+
+    return OutputArr;
+
+} 
+
+console.log(getUniquePrducts(listOfProducts));
 
