e2bb3cdc2a7b92daeae0b954576cc32f50e9cbdd
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-15 23:29:25 +0530

    separate hours and minutes done

---
+++ separateHoursAndMinutes.js
@@ -0,0 +1,48 @@
+function runProgram(input) {
+    input = input.trim();
+    let num = +input;
+    console.log(SeparateHoursMinutes(num));
+  }
+  
+  
+  
+function SeparateHoursMinutes(num) {
+
+if(num<60) {
+   return  `0:${num}`; 
+} else {
+    let x = Math.floor(num/60);
+    let y = num%60;
+    return (`${x}:${y}`);
+}
+
+
+}
+  
+  
+  
+  
+  
+  
+  
+ 
+  if (process.env.USERNAME === "ASUS") {
+    runProgram(`126`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
+    });
+  }
