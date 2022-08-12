e0c07004df30d9f98ae9e8a9e12de5682056632c
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-08-12 22:58:30 +0530

    prime identifier done

---
+++ primeIdentifier.js
@@ -0,0 +1,14 @@
+function identifyPrime(num) {
+    
+  var count = 0;
+    for (i=1; i<=num ;i++ ) {
+      if ( num % i === 0 ) {
+        count++;
+      }
+    } if ( count == 2 ) {
+      console.log("Yes");
+    } else {
+      console.log("No");
+    }
+        
+}
